a495c2d5de5ff746ccd90aa7ae1e9b6abb157304
DESKTOP-PJGE4NP\Studio-Desktop <joe.hunter.dev@gmail.com>
2023-07-10 16:00:21 +0200

    State for each input value and touchstate, isValid done inline

--- forms-user-input/src/components/BasicForm.js
+++ forms-user-input/src/components/BasicForm.js
@@ -2,32 +2,37 @@ import { useState } from 'react';
 
 const BasicForm = (props) => {
 
+  //firstName
   const [firstNameEntered, setFirstNameEntered] = useState(false)
+  const [firstNameIsTouched, setFirstNameIsTouched] = useState(false)
 
-  //instructor has done one for each field
-  const [isValid, setIsValid] = useState({})
-  // const [isTouched,setIsTouched] = useState(false)
-  const [isTouched, setIsTouched] = useState('')
-
-
-
-  // const [data,setData] = useState(false)
-  //inline implied: its own useState would be overkill
-  // const firstNameValid = firstNameEntered.trim() !== '';
+  //validation
   const firstNameEnteredIsValid = (/^([^0-9]*)$/i.test(firstNameEntered))
-  const firstNameInputIsInvalid = !firstNameEnteredIsValid && isTouched.includes("firstName");
+  const firstNameInputIsInvalid = !firstNameEnteredIsValid && firstNameIsTouched;
+
+  //lastName
+  const [lastNameEntered, setLastNameEntered] = useState(false)
+  const [lastNameIsTouched, setLastNameIsTouched] = useState(false)
+
+  //validation
+  const lastNameEnteredIsValid = (/^([^0-9]*)$/i.test(lastNameEntered))
+  const lastNameInputIsInvalid = !lastNameEnteredIsValid && lastNameIsTouched;
+
+  //email
+  const [emailEntered, setEmailEntered] = useState(false)
+  const [emailIsTouched, setEmailIsTouched] = useState(false)
+
+  //validation
+  const emailEnteredIsValid = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailEntered))
+  const emailInputIsInvalid = !emailEnteredIsValid && emailIsTouched;
 
 
   let formIsValid = false
 
 
-  if (firstNameEntered) {
+  if (firstNameEnteredIsValid && lastNameEnteredIsValid && emailEnteredIsValid) {
     formIsValid = true
   }
-
-  //also do touch
-
-  //check for touch
 
   //submit handler
   const submitHandler = (e) => {
@@ -38,90 +43,73 @@ const BasicForm = (props) => {
       console.log("go fetch")
     }
 
-    if (!firstNameEntered) {
-      formIsValid = false
-    }
-
+  if (!firstNameEnteredIsValid || !lastNameEnteredIsValid || !emailEnteredIsValid) {
+    return
+  }
     //reset
     setFirstNameEntered('')
-    setIsTouched(false);
+    setFirstNameIsTouched(false);
 
+    setLastNameEntered('')
+    setLastNameIsTouched(false);
+
+    setEmailEntered('')
+    setEmailIsTouched(false);
   }
 
-  //
+  //Hnadlers
   const firstNameChangeHandler = (event) => {
 
     setFirstNameEntered(event.target.value);
-
-    setIsValid(...event.target.id)
-  }
-  // const test = isValid.filter(t => t !== 'firstName')
-
-  // //This is a case for reducer () 
-  // const changeHandler = (event) => {
-  //   const firstNameEnteredIsValid = (/^([^0-9]*)$/i.test(firstNameEntered))
-
-  //   switch (event.target.id) {
-
-  //     case "lastName":
-
-  //       (/^([^0-9]*)$/i.test(event.target.value)) ? setIsValid([event.target.id]) : setIsValid((current) => current.filter(current) !== event.target.id)
-
-
-
-  //       break;
-
-
-
-  //     case "email":
-
-  //       (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value)) ? setIsValid([...prevState,event.target.id]) : setIsValid((current) => current.filter(current) !== event.target.id)
-
-  //       break;
-
-  //     default:
-
-  //   }
-  //   // keep all other key-value pairs
-  //   // update the value of specific key
-  // }
-
-
-
-  const blurHandler = (event) => {
-    // init = {inputId:''}
-    setIsTouched(event.target.id);
-    // console.log(isTouched === "firstName")
-
   }
 
-  // Generic Change handler =?
 
-  // const onChangeHandler = (event) => {
+  const firstNameBlurHandler = (event) => {
+    setFirstNameIsTouched(true);
+  }
 
-  //   setData({name:event.target.name,value: event.target.value})
+  const lastNameChangeHandler = (event) => {
 
-  // }
+    setLastNameEntered(event.target.value);
+  }
 
-  const className = formIsValid ? 'form-control invalid' : 'form-control';
+
+  const lastNameBlurHandler = (event) => {
+    setLastNameIsTouched(true);
+  }
+
+
+  const emailChangeHandler = (event) => {
+
+    setEmailEntered(event.target.value);
+  }
+
+
+  const emailBlurHandler = (event) => {
+    setEmailIsTouched(true);
+  }
+
+
+  // const className = formIsValid ? 'form-control invalid' : 'form-control';
 
   return (
     <form>
       <div className='control-group'>
         <div className={firstNameInputIsInvalid ? 'form-control invalid' : 'form-control'}>
           <label htmlFor='firstName'>First Name</label>
-          <input type='text' id='firstName' onBlur={blurHandler} onChange={firstNameChangeHandler} />
-          {isTouched.includes("firstName") && !firstNameEnteredIsValid && <p> Firstname is empty or invalid</p>}
+          <input type='text' id='firstName' onBlur={firstNameBlurHandler} onChange={firstNameChangeHandler} />
+          {firstNameInputIsInvalid && <p> Firstname is invalid</p>}
         </div>
-        <div className={isTouched.lastName ? 'form-control invalid' : 'form-control'}>
+        <div className={lastNameInputIsInvalid ? 'form-control invalid' : 'form-control'}>
           <label htmlFor='lastName'>Last Name</label>
-          <input type='text' id='lastName' onChange={changeHandler} onBlur={blurHandler} />
-          {/* {isTouched && !isValid.includes("lastName") &&  <p> Firstname is empty or invalid</p>} */}
+          <input type='text' id='lastName' onBlur={lastNameBlurHandler} onChange={lastNameChangeHandler} />
+          {lastNameInputIsInvalid && <p> Last name is invalid</p>}
         </div>
       </div>
-      <div className={isTouched.includes("email") ? 'form-control invalid' : 'form-control'}>
+      <div className={emailInputIsInvalid ? 'form-control invalid' : 'form-control'}>
         <label htmlFor='email'>E-Mail Address</label>
-        <input type='email' id='email' onChange={changeHandler} onBlur={blurHandler} />
+        <input type='email' id='email' onBlur={emailBlurHandler} onChange={emailChangeHandler} />
+        {emailInputIsInvalid && <p> Email is invalid</p>}
       </div>
       <div className='form-actions'>
         <button onSubmit={submitHandler} disabled={formIsValid}>Submit</button>
