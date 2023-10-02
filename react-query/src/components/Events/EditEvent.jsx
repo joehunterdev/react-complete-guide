f8670d566e771043bf2b992665c713c0cd5011e2
DESKTOP-PJGE4NP\Studio-Desktop <joe.hunter.dev@gmail.com>
2023-10-02 15:25:45 +0200

    Updating with mutations

--- react-query/src/components/Events/EditEvent.jsx
+++ react-query/src/components/Events/EditEvent.jsx
@@ -1,30 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { fetchEvent } from "../../util/http.js";
+import { fetchEvent, updateEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
+
 export default function EditEvent() {
 
   const navigate = useNavigate();
   const params = useParams();
-
-  // console.log("id" + params.id);
-
-  // const [data, isPending, isError, error] = useQuery({
-  //   queryKey: ["events", params.id],
-  //   queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
-  // });
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", { search: params.id }],
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
   });
 
-  // console.log(data);
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../");
+  }
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+  });
 
   function handleClose() {
     navigate("../");
@@ -60,14 +59,14 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-      <Link to="../" className="button-text">
-        Cancel
-      </Link>
-      <button type="submit" className="button">
-        Update
-      </button>
-    </EventForm>
-    )
+        <Link to="../" className="button-text">
+          Cancel
+        </Link>
+        <button type="submit" className="button">
+          Update
+        </button>
+      </EventForm>
+    );
   }
   console.log(content);
   return <Modal onClose={handleClose}>{content}</Modal>;
