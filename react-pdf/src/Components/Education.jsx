ed49bf2f32659eb161e6b42e9cf9c9b97bbd4f47
DESKTOP-PJGE4NP\Studio-Desktop <joe.hunter.dev@gmail.com>
2023-11-05 17:30:32 +0100

    internal awards

--- react-pdf/src/Components/Education.jsx
+++ react-pdf/src/Components/Education.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, View, Image, StyleSheet } from "@react-pdf/renderer";
-import { cleanText } from "../util/utils";
+import { cleanText, getFilenameFromUrl } from "../util/utils";
+
 const styles = StyleSheet.create({
   containerEducation: {
     fontSize: 12,
@@ -11,31 +12,59 @@ const styles = StyleSheet.create({
   containerColInner: {
     flexGrow: 2,
     flexWrap: "wrap",
-    padding:4,
-    border:"1px solid #ccc",
-    width:"30%"
+    padding: 4,
+    border: "1px solid #ccc",
+    width: "30%",
+  },
+  containerCertificates: {
+    textAlign: "left",
+    flexDirection: "row",
+  },
+  certImage: {
+    width: "180px",
   },
 });
 
 const Education = ({ education }) => {
+
+  if (!education || education.length === 0) {
+    
+    return null;
+  }
+  
   return (
-    <View style={styles.containerEducation} wrap={false}>
-      {education.map((item, index) => (
-        <View key={index} style={styles.containerColInner}>
-          <View>
-            <Text>{item.period}</Text>
+    <>
+      <View style={styles.containerEducation} wrap={false}>
+        {education.map((item, index) => (
+          <View key={index} style={styles.containerColInner}>
+            <View>
+              <Text>{item.period}</Text>
+            </View>
+            <View>
+              <Text>{item.titled}</Text>
+            </View>
+            <View style={styles.containerList}>
+              {item.awarded.map((award, awardIndex) => (
+                <Text key={awardIndex}>{award}</Text>
+              ))}
+            </View>
           </View>
-          <View>
-            <Text>{item.titled}</Text>
-          </View>
-          <View style={styles.containerList}>
-            {item.awarded.map((award, awardIndex) => (
-              <Text key={awardIndex}>{award}</Text>
+        ))}
+      </View>
+      <View style={styles.containerCertificates} wrap={false}>
+        {education.certificateImages && education.certificateImages.length > 0 && (
+          <>
+            {education.certificateImages.map((url, index) => (
+              <Image
+                key={index}
+                src={`../src/assets/image/certs/${getFilenameFromUrl(url)}`}
+                style={styles.certImage}
+              />
             ))}
-          </View>
-        </View>
-      ))}
-    </View>
+          </>
+        )}
+      </View>
+    </>
   );
 };
 
